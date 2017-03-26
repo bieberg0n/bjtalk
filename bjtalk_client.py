bdc860be87d11cb85ab9860df25d7692bef1d151
bjong <biebergong@gmail.com>
2017-03-26 13:15:55 +0800

    add file

---
+++ bjtalk_client.py
@@ -0,0 +1,46 @@
+import socket
+import pyaudio
+
+
+# CHUNK = 1024
+# WIDTH = 2
+# CHANNELS = 2
+# RATE = 44100
+# RECORD_SECONDS = 5
+
+
+def main():
+    p = pyaudio.PyAudio()
+    cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    cli.bind(('0.0.0.0', 20171))
+
+    # stream = p.open(format=p.get_format_from_width(WIDTH),
+    stream = p.open(format=1,
+                    channels=1,
+                    rate=8000,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=1024)
+
+    print("* recording")
+
+    # for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    try:
+        while True:
+            data = stream.read(1024)
+            cli.sendto(data, ('127.0.0.1', 20170))
+            print(len(data))
+            stream.write(data, 1024)
+
+    except KeyboardInterrupt:
+        pass
+
+    print("* done")
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
+main()
---
+++ bjtalk_server.py
@@ -0,0 +1,39 @@
+import socket
+import time
+import pprint
+
+
+# users = {
+# ('127.0.0.1', 20170):233,
+# ('127.0.0.2', 20170):234
+# }
+
+def handle(data, addr, users, serv):
+    [serv.sendto(data, user) for user in users if user != addr]
+    return
+
+
+def start_serv():
+    users = dict()
+    serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serv.bind(('0.0.0.0', 20170))
+    clock = int(time.time())
+
+    while True:
+        now = int(time.time())
+        if now - clock > 60:
+            users = {user: users[user] for user in users.keys() if (now-users[user]) < 60}
+            pprint.pprint(users)
+            clock = now
+
+        else:
+            pass
+
+        data, addr = serv.recvfrom(4096)
+        users[addr] = now
+        # print(len(data), addr, users)
+        handle(data, addr, users, serv)
+
+
+if __name__ == '__main__':
+    start_serv()
